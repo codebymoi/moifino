2329a1b65352a3557f45c62b60c6c1a55facd701
codebymoi <110585088+codebymoi@users.noreply.github.com>
2025-03-07 19:23:09 -0800

    Performance updates

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
+const loadColorEl = document.querySelector('.load-color');
+const logoEl = document.querySelector('.logo');
+const iconsEl = document.querySelector('.icons');
+const lqntEl = document.querySelector('.lqnt');
+const fuerteEl = document.querySelector('.fuerte');
 
 window.addEventListener('load', () => {
-    spinnerWrapperEl.style.opacity = '0';
+    loadColorEl.style.opacity = '0';
 
     setTimeout(() => {
-        spinnerWrapperEl.style.display = 'none';
-    }, 500);
+        loadColorEl.style.display = 'none';
+    }, 2000);
+
+    logoEl.style.animation = 'slide-in-left 500ms';
+    iconsEl.style.animation = 'zoom-in 500ms';
+    lqntEl.style.animation = 'slide-in-right 500ms';
+    fuerteEl.style.animation = 'slide-in-right 500ms';
 });
